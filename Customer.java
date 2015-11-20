cf0e4d3fc63add84d74c6255f5c776b4bc67ff14
kakarrot007 <kakarrot007@users.noreply.github.com>
2015-11-20 12:11:11 -0600

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,48 @@
+package qcollection;
+
+public class Customer implements BinaryPrioritizable {
+
+	   
+
+    private String name;
+
+    private int prevPurchaseAmt;
+
+   
+
+    public Customer(String name, int prevPurchaseAmt) {
+
+        this.name = name;
+
+        this.prevPurchaseAmt = prevPurchaseAmt;
+
+    }
+
+    // assign customers who have purchases over $1000 in the past high
+    // priority
+
+    @Override public int getPriority() {
+
+        if (prevPurchaseAmt >= 1000) {
+
+            return 1;
+
+        }
+
+        else {
+
+            return 0;
+
+        }
+
+    }
+
+   
+
+    @Override public String toString() {
+
+        return "(" + name + ", Prev Purchases: $" + prevPurchaseAmt + ")";
+
+    }  
+
+}
